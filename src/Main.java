c8258a60f554235e43923a011f754b32974833cc
Charlie David <davys4802@gmail.com>
2026-09-17 12:06:25 -0500

    Entire Program, {} corrections

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,11 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
+public class Main
+{
+
+    public static void main(String[] args) throws IOException
+    {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         double n1, n2, n3, n4, n5;
--- src/NumeroMayor.java
+++ src/NumeroMayor.java
@@ -1,4 +1,5 @@
-public class NumeroMayor {
+public class NumeroMayor
+{
 
     private double num1;
     private double num2;
@@ -6,7 +7,8 @@ public class NumeroMayor {
     private double num4;
     private double num5;
 
-    public NumeroMayor(double num1, double num2, double num3, double num4, double num5) {
+    public NumeroMayor(double num1, double num2, double num3, double num4, double num5)
+    {
         this.num1 = num1;
         this.num2 = num2;
         this.num3 = num3;
@@ -15,19 +17,24 @@ public class NumeroMayor {
     }
 
     // Versión 1: solo con sentencias if
-    public double mayorConIf() {
+    public double mayorConIf()
+    {
         double mayor = num1;
 
-        if (num2 > mayor) {
+        if (num2 > mayor)
+        {
             mayor = num2;
         }
-        if (num3 > mayor) {
+        if (num3 > mayor)
+        {
             mayor = num3;
         }
-        if (num4 > mayor) {
+        if (num4 > mayor)
+        {
             mayor = num4;
         }
-        if (num5 > mayor) {
+        if (num5 > mayor)
+        {
             mayor = num5;
         }
 
@@ -35,7 +42,8 @@ public class NumeroMayor {
     }
 
     // Versión 2: usando Math.max
-    public double mayorConMath() {
+    public double mayorConMath()
+    {
         double mayor = Math.max(num1, num2);
         mayor = Math.max(mayor, num3);
         mayor = Math.max(mayor, num4);
